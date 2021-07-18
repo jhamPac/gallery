96e18e42f3c979a0e317588e41a9f6cec72a88c9
jhamPac <cloud@kevia.me>
2021-07-18 12:37:14 -0700

    feat: func for creating user service

--- model/user.go
+++ model/user.go
@@ -1,9 +1,31 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type UserService struct {
+	db *gorm.DB
+}
 
 type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;unique_index"`
+}
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+func (us *UserService) Close() error {
+	return us.db.Close()
 }
