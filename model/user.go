42cd0a96d32220cf0078f98743814068ad77ef29
jhamPac <cloud@kevia.me>
2021-07-19 19:55:11 -0700

    feat: find by email

--- model/user.go
+++ model/user.go
@@ -32,21 +32,36 @@ func NewUserService(connectionInfo string) (*UserService, error) {
 	}, nil
 }
 
+func first(db *gorm.DB, dst interface{}) error {
+	err := db.First(dst).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrNotFound
+	}
+	return err
+}
+
 func (us *UserService) Create(user *User) error {
 	return us.db.Create(user).Error
 }
 
 func (us *UserService) ByID(id uint) (*User, error) {
 	var user User
-	err := us.db.Where("id = ?", id).First(&user).Error
-	switch err {
-	case nil:
-		return &user, nil
-	case gorm.ErrRecordNotFound:
-		return nil, ErrNotFound
-	default:
+	db := us.db.Where("id = ?", id)
+	err := first(db, &user)
+	if err != nil {
 		return nil, err
 	}
+	return &user, nil
+}
+
+func (us *UserService) ByEmail(email string) (*User, error) {
+	var user User
+	db := us.db.Where("email = ?", email)
+	err := first(db, &user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
 func (us *UserService) Close() error {
