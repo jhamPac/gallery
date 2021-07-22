db51165cfed141da3bb0079a8f302f25fd78e10f
jhamPac <cloud@kevia.me>
2021-07-21 22:20:05 -0700

    feat: add remember me token helper funcs

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ func main() {
 	r.Handle("/login", userC.LoginView).Methods("GET")
 	r.HandleFunc("/login", userC.Login).Methods("POST")
 
+	// development endpoint
 	r.HandleFunc("/cookietest", userC.CookieTest)
 
 	http.ListenAndServe(":8080", r)
---
+++ rando/rando.go
@@ -0,0 +1,23 @@
+package rando
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func String(nBytes int) (string, error) {
+	b, err := Bytes(nBytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
