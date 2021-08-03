1654303428f8aeb53802915ebc4cc5e29b41c97e
jhamPac <cloud@kevia.me>
2021-08-02 19:20:06 -0700

    refactor: function to white listed errors

--- view/data.go
+++ view/data.go
@@ -1,5 +1,7 @@
 package view
 
+import "log"
+
 const (
 	AlertLvlError   = "danger"
 	AlertLvlWarning = "warning"
@@ -17,4 +19,26 @@ type Data struct {
 type Alert struct {
 	Level   string
 	Message string
+}
+
+type PublicError interface {
+	error
+	Public() string
+}
+
+func (d *Data) SetAlert(err error) {
+	var msg string
+
+	pErr, ok := err.(PublicError)
+	if ok {
+		msg = pErr.Public()
+	} else {
+		log.Println(err)
+		msg = AlertMsgGeneric
+	}
+
+	d.Alert = &Alert{
+		Level:   AlertLvlError,
+		Message: msg,
+	}
 }
